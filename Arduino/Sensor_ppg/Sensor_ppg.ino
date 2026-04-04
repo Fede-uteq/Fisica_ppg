b711b00a4a19e165ccff5ccd128af6dce4cee2e6
Fedehl <fedehl632@gmail.com>
2026-04-03 21:29:06 -0600

    Creacion del proyecto

---
+++ Arduino/Sensor_ppg/Sensor_ppg.ino
@@ -0,0 +1,14 @@
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  /*
+    Realizar la adquisicion de la señal del sensor
+    Analizar señal y normalizar los valores para mandarlos al Vi
+  */
+
+}
